467555e6ae0daca3872452eea0b27737c9812ba4
Cassidy Horzempa <horzempac@gmail.com>
2019-02-16 23:07:02 -0500

    cleaned up imports

--- app/src/main/java/com/example/polygons/PolyActivity.java
+++ app/src/main/java/com/example/polygons/PolyActivity.java
@@ -3,28 +3,12 @@ package com.example.polygons;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CustomCap;
-import com.google.android.gms.maps.model.Dash;
-import com.google.android.gms.maps.model.Dot;
-import com.google.android.gms.maps.model.Gap;
-import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.PatternItem;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.RoundCap;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static com.example.polygons.R.id.map;
 
