04785727ffd499500253615a29d427f2d8842d67
lhw2553 <lhw2553@rit.edu>
2019-02-17 00:32:41 -0500

    Added tons of pins

--- app/src/main/java/com/example/polygons/PolyActivity.java
+++ app/src/main/java/com/example/polygons/PolyActivity.java
@@ -9,6 +9,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
 
 import static com.example.polygons.R.id.map;
 
@@ -47,7 +54,61 @@ public class PolyActivity extends AppCompatActivity
 
         // Position the map's camera near Alice Springs in the center of Australia,
         // and set the zoom factor so most of Australia shows on the screen.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.157, -77.609), 10));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.157, -77.609), 10));
+
+        LatLng rochester = new LatLng(43.106998, -77.659939);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.085114, -77.667220))
+                .title("Marker in NRH"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.086485, -77.667821))
+                .title("Marker in Ellingson"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.086148, -77.667403))
+                .title("Marker in Gibson"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.083468, -77.681637))
+                .title("Marker in Global Village"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.079947,-77.67814))
+                .title("Marker in Global Village"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.123096,-77.662103))
+                .title("Marker in Airport"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.122720,-77.663515))
+                .title("Marker in Airport"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.107558,-77.686679))
+                .title("Marker in Airport"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.120089,-77.658711))
+                .title("Marker in Airport"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.084116,-77.632076))
+                .title("Marker in Market Place Mall"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.083614,-77.636194
+        ))
+                .title("Marker in Market Place Mall"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.082110,-77.633406))
+                .title("Marker in Market Place Mall"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.102449,-77.610886))
+                .title("Marker in MCC"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.102292,-77.610714))
+                .title("Marker in MCC"));
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.100287,-77.607625))
+                .title("Marker in MCC"));
+
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rochester, 13));
+
+
 
     }
 }
+
